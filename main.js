d3426006e4f550818f7bc24e70f0142496085b7b
Marko Ilic <ivanstojanovic00@gmail.com>
2022-11-12 04:07:30 +0100

    refactoring, fixing the generate password logic

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const display = document.querySelector('.password')
 const length = document.querySelector('.passwordLength')
 const strength = document.querySelector('.diff')
-const levels = document.querySelector('.level')
+const firstLevel = document.querySelector('.levelOne')
+const secondLevel = document.querySelector('.levelTwo')
+const thirdLevel = document.querySelector('.levelThree')
+const fourthLevel = document.querySelector('.levelFour')
 const clipboard = document.getElementById('clipboard')
 const slider = document.querySelector('.slider')
 const includeUppercase = document.getElementById('uppercase')
@@ -12,11 +15,13 @@ const h3 = document.querySelector('h3')
 const h1 = document.querySelector('h1')
 let numbers = [0,1,2,3,4,5,6,7,8,9]
 let characters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+//let uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']testing, problems with .toUpperCase
 let symbols = ['@','#','$','%','^','&','*','(',')','_','+','?','<','>',':','{','}','[',']']
 let password = []
 let generateCounter = 0
 
 let passwordDisplay = [];
+let clickCounter = 0
 
 length.addEventListener('input', numberSync)
 slider.addEventListener('input', numberSync)
@@ -39,30 +44,66 @@ function copy() {
     display.select()
     navigator.clipboard.writeText(display.value)
 }
-
 clipboard.addEventListener('click', copy)
 
 function generatePassword() {
     for(let i = 0; i<length.value; i++){
+        if(includeNumbers.checked &&  i % 3 === 0){
+            passwordDisplay.push(numbers[Math.floor(Math.random() *10)])
+            console.log(display.value.length)      
+        }else if(includeSymbols.checked &&  i % 5 === 0 ){
+            passwordDisplay.push(symbols[Math.floor(Math.random() *19)])
+            console.log(display.value.length)      
+        }else if(includeUppercase.checked &&  i % 4 === 0){
+            passwordDisplay.push(characters[Math.floor(Math.random() *26)].toUpperCase())
+            console.log(display.value.length)      
+        }else{
         passwordDisplay.push(characters[Math.floor(Math.random() *26)])
-        console.log(passwordDisplay)
-    }if(includeNumbers.checked &&  i % 3 === 0){
-        passwordDisplay.push(numbers[Math.floor(Math.random() *10)])
-        console.lod(adanije)
+        }
     }
+     display.value = ''   
     display.value = passwordDisplay.join('')
     display.innerText = display.value
-  
+    console.log(display.value.length)
+    console.log(clickCounter)
+    clickCounter++
     
-}{once:true}
+    
+    
+        //Changing the password strength indicator
+        if(length.value >= 8 && length.value <= 12){
+            strength.innerText = 'Weak'
+            strength.style.color = 'red'
+            firstLevel.style.backgroundColor = 'red'
+            secondLevel.style.backgroundColor = 'transparent'
+            thirdLevel.style.backgroundColor = 'transparent'
+            fourthLevel.style.backgroundColor = 'transparent'
+        }else if(length.value >= 12 && length.value <= 17){
+            strength.innerText = 'Medium'
+            strength.style.color = 'yellow'
+            firstLevel.style.backgroundColor = 'red'
+            secondLevel.style.backgroundColor = 'yellow'
+            thirdLevel.style.backgroundColor = 'transparent'
+            fourthLevel.style.backgroundColor = 'transparent'
+        }else if(length.value >= 17 && length.value <= 25){
+            strength.innerText = 'Strong'
+            strength.style.color = 'orange'
+            firstLevel.style.backgroundColor = 'red'
+            secondLevel.style.backgroundColor = 'yellow'
+            thirdLevel.style.backgroundColor = 'orange'
+            fourthLevel.style.backgroundColor = 'transparent'
+        }else  if(length.value >= 25 && length.value <= 30){
+            strength.innerText = 'Perfect!'
+            strength.style.color = 'green'
+            firstLevel.style.backgroundColor = 'red'
+            secondLevel.style.backgroundColor = 'yellow'
+            thirdLevel.style.backgroundColor = 'orange'
+            fourthLevel.style.backgroundColor = 'green'
+        }
+
+    }
 
 generate.addEventListener('click', generatePassword)
-
-
-
-
-
-
 
 
 
